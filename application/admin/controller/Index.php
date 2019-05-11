ec11483afe639f9202d1d84baee620d7f0633c71
wuchuheng <wuchuheng@163.com>
2019-05-12 02:31:55 +0800

    the  first  demo page  for admin was success

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -5,8 +5,22 @@ use think\Controller;
 
 class Index extends Controller
 {
+    /**
+     * 左则菜单栏
+     *
+     */
     public function index()
     {
         return $this->fetch();
+    }
+
+
+    /**
+     * 首页
+     *
+     */
+    public function layout()
+    {
+        return $this->fetch(); 
     }
 }
