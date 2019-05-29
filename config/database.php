eed2a1b07732d97e810535d55b0d5b2f88a127e0
wuchuheng <wuchuheng@163.com>
2019-05-29 09:41:12 -0700

    it was  gone 47% to develop the  feature  to add a new member

--- config/database.php
+++ config/database.php
@@ -13,7 +13,7 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => 'phpmyadmin.wuchuheng.com',
+    'hostname'        => '127.0.0.1',
     // 数据库名
     'database'        => 'electronchat',
     // 用户名
